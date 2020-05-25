eada1e97b6038858a92a2d8d9eb2a4c9e6fc8f45
Alex Bowers <alex.bowers@bluebaytravel.co.uk>
2020-05-25 13:36:44 +0100

    Fix what is being returned in getUser

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -4,15 +4,13 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use GrahamCampbell\Binput\Facades\Binput;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function getUser() {
-        if ($user = Binput::get('user')){
-            return User::find($user);
-        }
+    public function getUser(User $user) {
+        if ($user) return $user;
+
         return Auth::user();
     }
 }
